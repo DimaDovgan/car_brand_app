5067d50bf33a4ca7ccd1f631a788fd4ba66e2446
DimaDovgan <89789155+DimaDovgan@users.noreply.github.com>
2025-06-22 17:19:04 +0300

    try  fix CORS

--- src/app/api/(cars)/test/route.ts
+++ src/app/api/(cars)/test/route.ts
@@ -2,8 +2,13 @@ import {NextResponse } from 'next/server';
 
 export async function GET() {
   try {
+    const response = NextResponse.json("It is good deal ", {status: 200});
+response.headers.set('Access-Control-Allow-Origin', 'https://car-brand-app-git-main-dmitrys-projects-95a88bf3.vercel.app/');
+    response.headers.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+    
+    return response;
 
-    return NextResponse.json("It is good deal ",{status:200});
     
   } catch (error) {
     console.error("Помилка отримання брендів: ", error);
