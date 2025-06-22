e6da39dae74bc21ac52822028d52e847f400739d
DimaDovgan <89789155+DimaDovgan@users.noreply.github.com>
2025-06-22 17:22:51 +0300

    try  fix *

--- src/app/api/(cars)/test/route.ts
+++ src/app/api/(cars)/test/route.ts
@@ -3,7 +3,7 @@ import {NextResponse } from 'next/server';
 export async function GET() {
   try {
     const response = NextResponse.json("It is good deal ", {status: 200});
-response.headers.set('Access-Control-Allow-Origin', 'https://car-brand-app-git-main-dmitrys-projects-95a88bf3.vercel.app/');
+response.headers.set('Access-Control-Allow-Origin', '*');
     response.headers.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
     response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
     
